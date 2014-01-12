950fcb402676c1e2058c4954591fe0fda61150a5
Rick Sladkey <rick.sladkey@gmail.com>
2014-01-12 18:10:22 -0500

    Workaround for autocrlf problem.

--- GitDiffMargin/Git/GitCommands.cs
+++ GitDiffMargin/Git/GitCommands.cs
@@ -86,8 +86,11 @@ namespace GitDiffMargin.Git
 
             if (autocrlf == null || !autocrlf.Value) return content;
 
+#if false
+            // Figure out why this doesn't work.
             var contentText = Encoding.UTF8.GetString(content);
             content = Encoding.UTF8.GetBytes(contentText.Replace("\r\n", "\n"));
+#endif
             return content;
         }
 
